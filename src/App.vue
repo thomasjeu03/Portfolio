55a951a699aa338a571b5673f48c68cc4cf8cf87
thomasjeu03 <jeuthomas03@gmail.com>
2022-03-15 12:32:54 +0100

    portfolio fini

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
+
     <header>
       <div class="header_content">
-        <div class="pp"></div>
-        <!--        <script>
-                  var hoverDistort = new hoverEffect({
-                    parent: document.querySelector('.pp'),
-                    intensity: 0.3,
-                    image1: './assets/img/ThomasJeu.png',
-                    image2: './assets/img/ThomasJeu2.png',
-                    displacementImage: './assets/img/diss.png',
-                  });
-                </script>-->
-        <img class="pp" src="../static/img/ThomasJeu.png" alt="">
-        <div class="circle"></div>
 
         <div class="row">
-          <h1>Thoomas Jeuu</h1>
+          <h1 id="h1">Thoomas Jeuu</h1>
         </div>
 
-        <h3>I'm a <b>18 years old</b> guy<br/>
-          As a <b>web</b>'s <b>multi-tool</b></h3>
+        <h3 id="h3">I'm a <b>18 years old</b> guy<br/>
+          As a creative <b>front-end</b> developer</h3>
       </div>
-      <a href="#main">
+      <a id="scrollDown" href="#main">
         <svg class="scroll_down" xmlns="http://www.w3.org/2000/svg" width="25.12" height="129.193" viewBox="0 0 25.12 129.193">
           <g id="scroll_down" transform="translate(-1794.88 -937)">
             <text id="Scroll_down-2" data-name="Scroll down" transform="translate(1799 1032) rotate(-90)" fill="#EEEAE0" font-size="14" font-family="SpaceMono-Regular, Space Mono"><tspan x="0" y="16">Scroll down</tspan></text>
@@ -34,6 +23,9 @@
         <span class="txt">WEB DEVELOPPMENT - GRAPHIC DESIGNN - AUDIOOVISUAL -&nbsp;</span>
         <span class="txt">WEB DEVELOPPMENT - GRAPHIC DESIGNN - AUDIOOVISUAL -&nbsp;</span>
       </div>
+
+
+
     </header>
 
     <router-view/>
@@ -51,4 +43,7 @@ export default {
 
 </script>
 
-<style></style>
+<style>
+
+
+</style>
--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -3,12 +3,17 @@
     <main id="main">
       <section>
         <h2>My lattest proojects</h2>
-        <div class="list_projets">
-          <div v-for="realisation in listeProjets" :key="realisation.id" data-aos="fade-up" class="projet">
+        <div class="list_projets" id="listeProjet">
+          <div v-for="realisation in listeProjets" :key="realisation.id"
+               data-aos="fade-up"
+               data-aos-easing="ease-out-cubic"
+               data-aos-delay="150"
+               data-aos-duration="1000"
+               class="projet" id="projet" :id="realisation.acf.id">
             <a class="haut-projet" :href="realisation.acf.url" target="_blank" rel="noopener">
-              <div :style='{ backgroundImage: `url(${realisation.acf.image})`}' class="wrappers"></div>
+              <div :style='{ backgroundImage: `url(${realisation.acf.imagetext})`}' class="wrappers"></div>
             </a>
-            <div class="bas_projet">
+            <div class="bas_projet" :style='{ background: `linear-gradient(280deg, ${realisation.acf.color}55, #5a3b4022 )` }'>
               <i class="projects_type">{{ realisation.acf.type }}</i>
               <br>
               <p class="projects_title">{{ realisation.acf.name }}</p>
@@ -49,13 +54,13 @@
         <div class="left">
           <h2>Get inn touch</h2>
           <br>
-          <form class="contact-form" action="" method="POST">
+          <form class="contact-form" @submit="checkForm">
             <div class="haut">
-              <input type="text" id="name" name="name" placeholder="Your name">
-              <input type="email" id="mail" name="mail" placeholder="Your email" required>
+              <input v-model="form.name" type="text" id="name" name="name" placeholder="Your name">
+              <input v-model="form.email"  type="email" id="mail" name="mail" placeholder="Your email" required>
             </div>
-            <textarea name="message" id="msg" placeholder="Your message" required></textarea>
-            <button class="button" type="submit" name="submit">Sennd it</button>
+            <textarea v-model="form.message" name="message" id="msg" placeholder="Your message" required></textarea>
+            <button class="button" type="submit" value="Submit" name="submit">Sennd it</button>
           </form>
         </div>
         <div class="text-right">
@@ -70,19 +75,19 @@
           <b><a href="mailto: contact@thomasjeu.fr">📧 contact@thomasjeu.fr</a></b>
           <br>
           <br>
-          <b><a href="static/Curriculum_Vitae_Thomas_JEU.pdf" download="">📜 download my CV</a></b>
+          <b><a href="static/Curriculum_Vitae_Thomas_JEU.pdf" download="">📜 download my resume</a></b>
         </div>
       </section>
       <footer>
-        <p style="color: #fd5530; font-weight: bold;">Thomas Jeu <bleu>2021</bleu></p>
+        <p style="color: #fd5530; font-weight: bold;">Thomas Jeu <span>2022</span></p>
         <a href="mailto: contact@thomasjeu.fr" style="color: #fd5530; font-weight: bold;">contact@thomasjeu.fr</a>
-        <div class="cursor"></div>
       </footer>
     </main>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 import param from '@/param/param'
 
@@ -90,8 +95,43 @@ export default {
   name: 'Accueil',
   data () {
     return {
-      liste:[]
+      liste:[],
+      form :{
+        name: null,
+        email: null,
+        message: null
+      }
     }
+  },
+
+  methods: {
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+    checkForm: function (e) {
+      e.preventDefault();
+
+        var bodyFormData = new FormData();
+        bodyFormData.set("your-name", this.form.name)
+        bodyFormData.set("your-mail", this.form.email);
+        bodyFormData.set("your-msg", this.form.message);
+
+        axios({
+          method: "post",
+          url:
+            "https://thomasjeu.fr/wp-json/contact-form-7/v1/contact-forms/28833/feedback",
+          data: bodyFormData,
+          config: { headers: { "Content-Type": "multipart/form-data" } },
+        })
+          .then(function (response) {
+            console.log(response);
+            window.location.href = "https://thomasjeu.fr";
+          })
+          .catch(function (response) {
+            console.log(response);
+          });
+    },
   },
 
   computed:{
@@ -116,5 +156,10 @@ export default {
 </script>
 
 <style scoped>
-
+.bas_projet{
+  border-radius: 0 0 20px 0;
+}
+.projet:hover .haut-projet{
+  border: 1px solid #fd5530;
+}
 </style>
